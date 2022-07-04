8dec1bea33d7e3ec7f78ef205991007be2b813dc
Maashn5 <108595873+Maashn5@users.noreply.github.com>
2022-07-04 03:01:47 +0300

    Update StepperMotor_WithPotentiometer.ino

--- StepperMotor_WithPotentiometer/StepperMotor_WithPotentiometer.ino
+++ StepperMotor_WithPotentiometer/StepperMotor_WithPotentiometer.ino
@@ -3,7 +3,7 @@
 #define steps 200
 
 
-Stepper StepperMotor(steps,2,3,4,5);
+Stepper StepperMotor(steps,2,3,4,5); // declare a stepper motor & define its ports in the arduino
 
 double Voltage;
 double NextStep;
@@ -12,17 +12,17 @@ double Angle;
 
 void setup()
 {
-  StepperMotor.setSpeed(60);
+  StepperMotor.setSpeed(60); // setting the speed of the steps of the motor
 }
 
 void loop()
 {
-  Voltage = analogRead(A3);
+  Voltage = analogRead(A3); // reading the voltage of the potentiometer
   
-  NextStep= map(Voltage,0,1023,0,200);
+  NextStep= map(Voltage,0,1023,0,200); // rescale the voltage into steps
     
-  Angle= NextStep - PastStep;
-  StepperMotor.step(Angle);
-  PastStep=NextStep;
+  Angle= NextStep - PastStep; // take the difference between the old position & the new one 
+  StepperMotor.step(Angle); // moving the stepper motor to its new position
+  PastStep=NextStep; // now the new position become the new past position 
   delay(2000);
 }
